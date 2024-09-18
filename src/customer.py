c1961cc2600c6f275066cceb2f3d9cabfb336a1b
dyrhave <126078101+dyrhave@users.noreply.github.com>
2024-09-18 13:07:59 +0200

    implenent crud functionality

--- src/customer.py
+++ src/customer.py
@@ -1,20 +1,40 @@
 import json
 
-# Hent kundedata fra json
-with open('../data/customers.json', 'r') as f:
-    customers = json.load(f)
-
 # Kunde
 class Customer:
-    def __init__(self, id, name, email, passwordHash):
-        self.id = id
+    _next_id = 1
+
+    def __init__(self, name, email, passwordHash, cust_id=None):
+        self.cust_id = cust_id
         self.name = name
         self.email = email
         self.passwordHash = passwordHash
 
+    @classmethod
+    def _get_next_id(cls):
+        current_id = cls._next_id
+        cls._next_id += 1
+        return current_id
+
+    @classmethod
+    def load_from_file(cls):
+        try:
+            with open('customers.json', 'r') as f:
+                customers = json.load(f)
+            if customers:
+                cls._next_id = max(customer['inv_id'] for customer in customers) + 1
+                return customers
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file(cls, customers):
+        with open('customers.json', 'w') as f:
+            json.dump(customers, f, indent=2)
+
     def to_dict(self):
         return {
-            "id": self.id,
+            "cust_id": self.cust_id,
             "name": self.name,
             "email": self.email,
             "passwordHash": self.passwordHash
@@ -22,25 +42,42 @@ class Customer:
 
     @classmethod
     def create(cls, name, email, passwordHash):
-        ##kode her##
+        new_customer = cls(name, email, passwordHash)
+        customers = cls.load_from_file()
+        customers.append(new_customer.to_dict())
+        cls.save_to_file(customers)
+        return new_customer
 
     @classmethod
     def get(cls, id):
-        ##kode her##
-
-
-    @classmethod
-    def update(cls, name, email, passwordHash):
-        ##kode her##
-
+        customers = cls.load_from_file()
+        for customer in customers:
+            if customer['id'] == id:
+                return cls(**customer)
+            return None
 
     @classmethod
-    def delete(cls, id):
-        ##kode her##
+    def update(cls, cust_id, **kwargs):
+        customers = cls.load_from_file()
+        for customer in customers:
+            if customer['cust_id'] == cust_id:
+                customer.update(**kwargs)
+                cls.save_to_file(customers)
+                return customer
+        return None
 
+    @classmethod
+    def delete(cls, cust_id):
+        customers = cls.load_from_file()
+        initial_customers = len(customers)
 
+        customers = [customer for customer in customers if customer['cust_id'] == cust_id]
+        if len(customers) < initial_customers:
+            cls.save_to_file(customers)
+            return True
+        else:
+            return False
 
-
-customer = Customer("C004", "John Doe", "<EMAIL>", "<PASSWORD>" )
-customer.to_dict()
-print(customer.to_dict())
+    @classmethod
+    def get_all(cls, customers):
+        return [cls(**customer) for customer in customers]
--- src/inventory.py
+++ src/inventory.py
@@ -1,19 +1,41 @@
 import json
 
-with open('inventory.json') as json_data:
-    inventory = json.load(json_data)
-
+# Lager
 class Inventory:
-    def __init__(self, id, brand, name, price, quantity):
-        self.id = id
+    _next_id = 1
+
+    def __init__(self, brand, name, price, quantity, inv_id=None):
+        self.inv_id = inv_id if inv_id is not None else self._get_next_id()
         self.brand = brand
         self.name = name
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def _get_next_id(cls):
+        current_id = cls._next_id
+        cls._next_id += 1
+        return current_id
+
+    @classmethod
+    def load_from_file(cls):
+        try:
+            with open('../data/inventory.json', 'r') as f:
+                inventory = json.load(f)
+            if inventory:
+                cls._next_id = max(item['inv_id'] for item in inventory) + 1
+                return inventory
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file(cls, inventory):
+        with open('../data/inventory.json', 'w') as f:
+            json.dump(inventory, f, indent=2)
+
     def to_dict(self):
         return {
-            "id": self.id,
+            "inv_id": self.inv_id,
             "brand": self.brand,
             "name": self.name,
             "price": self.price,
@@ -22,16 +44,42 @@ class Inventory:
 
     @classmethod
     def create(cls, brand, name, price , quantity):
-        ##Kode her##
+        new_item = cls(brand, name, price, quantity)
+        inventory = cls.load_from_file()
+        inventory.append(new_item.to_dict())
+        cls.save_to_file(inventory)
+        return new_item
 
     @classmethod
-    def get(cls, id):
-        ##Kode her##
+    def get(cls, inv_id):
+        inventory = cls.load_from_file()
+        for item in inventory:
+            if item['inv_id'] == inv_id:
+                return cls(**item)
+            return None
 
     @classmethod
-    def update(cls, brand, name, price , quantity):
-        ##Kode her##
+    def update(cls, inv_id, **kwargs):
+        inventory = cls.load_from_file()
+        for item in inventory:
+            if inv_id == item['inv_id']:
+                item.update(**kwargs)
+                cls.save_to_file(inventory)
+                return item(**item)
+        return None
 
     @classmethod
-    def delete(cls, id):
-        ##Kode her##
+    def delete(cls, inv_id):
+        inventory = cls.load_from_file()
+        initial_inv = len(inventory)
+
+        inventory = [item for item in inventory if item['inv_id'] != inv_id]
+        if len(inventory) < initial_inv:
+            cls.save_to_file(inventory)
+            return True
+        else:
+            return False
+
+    @classmethod
+    def get_all(cls, inventory):
+        return [cls(**item) for item in inventory]
